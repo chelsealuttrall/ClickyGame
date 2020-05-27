8077b5da44ce44e201d60b688669e42657ad6683
chelsealuttrall <chelsealuttrall@gmail.com>
2020-05-26 19:10:36 -0600

    Updates

---
+++ precache-manifest.6e4a48e517b6af52342b5e04d9864c08.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5671874bcc0c5b194aaafbeccc8cbcb1",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "44e23c673f84a7defa60",
+    "url": "/ClickyGame/static/css/main.08ab7604.chunk.css"
+  },
+  {
+    "revision": "51f2d3cbdfc27d0564c3",
+    "url": "/ClickyGame/static/js/2.1c5df226.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/ClickyGame/static/js/2.1c5df226.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44e23c673f84a7defa60",
+    "url": "/ClickyGame/static/js/main.fd58a52e.chunk.js"
+  },
+  {
+    "revision": "e4e45dfed9c9e7d57c47",
+    "url": "/ClickyGame/static/js/runtime-main.c063169d.js"
+  },
+  {
+    "revision": "11c3857f0560a0d113bcbeb3297d1d50",
+    "url": "/ClickyGame/static/media/Blot1.11c3857f.jpg"
+  },
+  {
+    "revision": "896ed59e78fc7b4f98dc96c56b94fd8e",
+    "url": "/ClickyGame/static/media/Blot2.896ed59e.jpg"
+  },
+  {
+    "revision": "0de331bc3b4b63483074f0aefa885fea",
+    "url": "/ClickyGame/static/media/Blot3.0de331bc.jpg"
+  },
+  {
+    "revision": "f6d5bbf3cc91173e15a4569466dc44ba",
+    "url": "/ClickyGame/static/media/Blot4.f6d5bbf3.jpg"
+  },
+  {
+    "revision": "f50243775d9124545a761134147ddbf6",
+    "url": "/ClickyGame/static/media/Blot5.f5024377.jpg"
+  },
+  {
+    "revision": "0b83843828d3912a1eef14e779d010d0",
+    "url": "/ClickyGame/static/media/Blot6.0b838438.jpg"
+  },
+  {
+    "revision": "3a071c8069ceaa734de979c9f3900f97",
+    "url": "/ClickyGame/static/media/Blot7.3a071c80.jpg"
+  },
+  {
+    "revision": "6e80ea02eaabae60c60c8db5dd1e544b",
+    "url": "/ClickyGame/static/media/Blot8.6e80ea02.jpg"
+  },
+  {
+    "revision": "34295a9886d005e43af229a22bfbce17",
+    "url": "/ClickyGame/static/media/Blot9.34295a98.jpg"
+  }
+]);
